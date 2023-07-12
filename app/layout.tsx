1c1e42b0016949649ae09a08bedaf5f664054c92
Yossef-Dawoad <yossefdawoad15@gmail.com>
2023-07-13 01:24:27 +0300

    add the  default `./global.css` file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-// import './globals.css'
+import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Startup Booster | AI Generated Marketings',
-  description: 'Create a powerful start-up branding campaign with creative names, catchy taglines, relevant keywords and profitable revenue streams for your business or product',       
+  description: 'Create a powerful start-up branding campaign with creative names, catchy taglines, relevant keywords and profitable revenue streams for your business or product',
   icons: ['/favicon.ico']
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import StartupBooster from './components/startupbooster'
 
 export default function Home() {
   return (
- 
+
     <StartupBooster />
 
   )
